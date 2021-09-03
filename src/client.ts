632e00ec512d875ddc373b966f7668fc409a160e
Julian Toscani <toscani@cekom.de>
2021-09-03 16:47:51 +0200

    setup client for testing

--- src/client.ts
+++ src/client.ts
@@ -7,8 +7,11 @@ const HOST = '127.0.0.1';
 const message = Buffer.from('My KungFu is Good!');
 
 const client = dgram.createSocket('udp4');
+
 client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
   if (err) throw err;
   console.log('UDP message sent to ' + HOST +':'+ PORT);
   client.close();
 });
+
+console.log("sent message!")
