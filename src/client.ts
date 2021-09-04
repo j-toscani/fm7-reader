546124365969fbbb72c135a6d6a716d33be864cf
Julian Toscani <julian_toscani@gmx.de>
2021-09-04 13:26:39 +0200

    merge with remote branch

--- src/client.ts
+++ src/client.ts
@@ -1,17 +1,17 @@
-import dgram from 'dgram';
-import {Buffer} from 'buffer';
+import dgram from "dgram";
+import { Buffer } from "buffer";
 
 const PORT = 33333;
-const HOST = '127.0.0.1';
+const HOST = "192.168.0.171";
 
-const message = Buffer.from('My KungFu is Good!');
+const message = Buffer.from("My KungFu is Good!");
 
-const client = dgram.createSocket('udp4');
+const client = dgram.createSocket("udp4");
 
 client.send(message, 0, message.length, PORT, HOST, sendMessage);
 
 function sendMessage(err: Error | null, bytes: number) {
   if (err) throw err;
-  console.log('UDP message sent to ' + HOST +':'+ PORT);
+  console.log("UDP message sent to " + HOST + ":" + PORT);
   client.close();
 }
--- src/lib/handler.ts
+++ src/lib/handler.ts
@@ -1,8 +1,7 @@
 import { RemoteInfo } from "dgram";
 
-
 export function onMessage(message: Buffer, remote: RemoteInfo) {
-  const mode = remote.size === 232 ? "sled" : "cardash"
+  const mode = remote.size === 232 ? "sled" : "cardash";
   const status = `${remote.address} : ${remote.port} - ${remote.size}`;
   console.log(status);
 }
