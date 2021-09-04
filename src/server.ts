22f919f3e9ba3db87349eff437b1069f34df27a0
Julian Toscani <julian_toscani@gmx.de>
2021-09-04 13:46:42 +0100

    remove http test server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import dgram from "dgram";
-import http, { IncomingMessage, ServerResponse } from 'http';
 import { onMessage } from "./lib/handler";
 import { createOnListening } from "./lib/handlerFactories";
 
@@ -8,18 +7,10 @@ const server = dgram.createSocket("udp4");
 const PORT = 33333;
 const HOST = "0.0.0.0";
 
-const requestListener = function (req:IncomingMessage, res:ServerResponse) {
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
-
-const httpServer = http.createServer(requestListener)
-
 server.on("listening", createOnListening(server));
 
 server.on("message", onMessage);
 
 export function startServer() {
   server.bind(PORT, HOST);
-  // httpServer.listen(PORT)
 }
