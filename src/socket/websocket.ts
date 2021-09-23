c35a782fb2f8743ccf7bf7756a2a32952fc26bd8
Julian Toscani <julian_toscani@gmx.de>
2021-09-23 22:17:28 +0200

    overhaul message acceptance

--- src/socket/websocket.ts
+++ src/socket/websocket.ts
@@ -3,38 +3,47 @@ import http from "http";
 // Error is expected as types are not correct! -> https://github.com/websockets/ws/issues/1932 -> https://github.com/DefinitelyTyped/DefinitelyTyped/pull/55151
 // @ts-expect-error
 import WebSocket, { WebSocketServer } from "ws";
-import decodeBuffer, { createDecoder } from "../lib/decodeBuffer";
-import { decodedBufferToString } from "../lib/onMessage";
-import { RaceDataStream } from "../lib/RaceDataStream";
+import UserRepo, { User } from "../db/repos/User";
+import RaceDataRawRepo from "../db/repos/RaceDataRaw";
 
-const socketServer = http.createServer(function (request, response) {
-  console.log(new Date() + " Received request for " + request.url);
+const connections: { [key: string]: RaceDataRawRepo } = {};
+
+const socketServer = http.createServer((request, response) => {
   response.writeHead(404);
   response.end();
 });
 
 const wss = new WebSocketServer({ server: socketServer });
 
-wss.on("connection", (ws: WebSocket, request: any) => {
-  const stream = new RaceDataStream(
-    {
-      address: request.headers.host,
-      family: "IPv4",
-      port: 5000,
-      size: 321,
-    },
-    () => {
-      console.log("finished");
-    }
-  );
-  ws.on("message", (message) => {
-    if (message instanceof Buffer) {
-      const decoder = createDecoder(message);
+function setUpWSS() {
+  wss.on("connection", async (ws: WebSocket, request: Request) => {
+    const user = await getUser(request);
 
-      const messageString = decodeBuffer(message, decoder);
-      stream.write(decodedBufferToString(messageString));
+    if (user) {
+      ws.on("message", createOnMessage(user));
+      ws.on("close", () => {
+        delete connections[user.email];
+      });
     }
   });
-});
+}
+
+async function getUser(request: Request) {
+  const { email } = request.body;
+  const repo = new UserRepo();
+  return repo.getOne({ email });
+}
+
+function createOnMessage(user: User) {
+  const dataRepo = new RaceDataRawRepo();
+  connections[user.email] = dataRepo;
+  dataRepo.create({ user, data: [], started: new Date() });
+
+  return (message: Buffer | string) => {
+    if (message instanceof Buffer) {
+      dataRepo.updateData(user.email, message);
+    }
+  };
+}
 
 export default socketServer;
