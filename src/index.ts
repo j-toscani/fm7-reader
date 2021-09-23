92c83259301714882e37ecc570e0206d58cbaacd
Julian Toscani <julian_toscani@gmx.de>
2021-09-23 22:21:39 +0200

    start ws-server after db connection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import dgram from "dgram";
 import dotenv from "dotenv";
 import logger from "./utils/logger";
 import socketServer from "./socket/websocket";
@@ -6,13 +5,17 @@ import { connectDatabase } from "./db";
 
 dotenv.config();
 
-const UDP_PORT = parseInt(process.env.UDP_PORT || "33333");
 const WS_PORT = parseInt(process.env.WS_PORT || "33333");
 
 const DB_PW = process.env.DB_PW ?? "admin";
 const DB_USER = process.env.DB_USER ?? "admin";
 
-const DB_URL = `mongodb://${DB_USER}:${DB_PW}@fm7-db:27017/?authSource=admin`; //   process.env.DB_URL ||
-// const HOST = "0.0.0.0";
+const DB_URL =
+  process.env.DB_URL ||
+  `mongodb://${DB_USER}:${DB_PW}@fm7-db:27017/?authSource=admin`;
 
-connectDatabase(DB_URL).then(() => {});
+connectDatabase(DB_URL).then(() => {
+  logger.info("Connected to Database!");
+
+  socketServer.listen(WS_PORT);
+});
