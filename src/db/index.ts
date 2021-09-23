24cf48ce21f9dab2868e0f415bef8a5d4471d35e
Julian Toscani <julian_toscani@gmx.de>
2021-09-23 22:16:15 +0200

    refactor db script

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,29 +1,12 @@
-import { Collection, MongoClient, Document } from "mongodb";
-import logger from "../utils/logger";
+import { Collection, MongoClient } from "mongodb";
 
-export default function connectToDb(
-  url: string,
-  onConnection: (client: Collection<Document>) => void
-) {
-  MongoClient.connect(url, async (err, client) => {
-    if (err) {
-      logger.error(err);
-      return;
-    }
-    if (client) {
-      logger.info("Connected to Database!");
-      const db = client.db("fm7_raw");
-      const connections = db.collection("connections");
+let client: MongoClient;
 
-      try {
-        await connections.insertOne({ timestamp: Date.now() });
-        logger.info("Created DB-Entry");
+export async function connectDatabase(url: string): Promise<void> {
+  client = new MongoClient(url);
+  await client.connect();
+}
 
-        const collection = db.collection("race_data");
-        onConnection(collection);
-      } catch (error) {
-        logger.error(error);
-      }
-    }
-  });
+export function getCollection<T>(name: string): Collection<T> {
+  return client.db("fm7").collection<T>(name);
 }
