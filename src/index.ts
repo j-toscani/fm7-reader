263e2f989701a1b49754003ac3f7804f6836a429
Julian Toscani <julian_toscani@gmx.de>
2021-10-10 20:16:21 +0200

    improve logging

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { connectDatabase } from "./db";
 
 dotenv.config();
 
-const WS_PORT = parseInt(process.env.WS_PORT || "33333");
+const WS_PORT = parseInt(process.env.WS_PORT || "33332");
 
 const DB_PW = process.env.DB_PW ?? "admin";
 const DB_USER = process.env.DB_USER ?? "admin";
@@ -14,8 +14,11 @@ const DB_URL =
   process.env.DB_URL ||
   `mongodb://${DB_USER}:${DB_PW}@fm7-db:27017/?authSource=admin`;
 
-connectDatabase(DB_URL).then(() => {
-  logger.info("Connected to Database!");
-
-  socketServer.listen(WS_PORT);
-});
+connectDatabase(DB_URL)
+  .then(() => {
+    logger.info("Connected to Database!");
+    socketServer.listen(WS_PORT, () =>
+      logger.info(`Server started on: ${WS_PORT}`)
+    );
+  })
+  .catch((err) => logger.error(err));
